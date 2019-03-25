d9cd025f54318e0497159527731acb09903794d0
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-25 19:59:32 +0100

    Add project files.

---
+++ GyufaKonya/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GyufaKonya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
