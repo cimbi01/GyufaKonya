d1fed620c92bdafda4dc9dbfb7241a28a08d5afc
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-25 20:18:42 +0100

    CodeMaid Clean

--- GyufaKonya/Program.cs
+++ GyufaKonya/Program.cs
@@ -2,28 +2,17 @@
 
 namespace GyufaKonya
 {
-    class Program
+    internal class Program
     {
-        private static int gyufaszam = 20;
+        #region Private Fields
+
         private readonly static int jatekosszam = 5;
-        static void Main(string[] args)
-        {
-            // gyufaszámból elveszünk 1/2/3 db-t > input amig a gyufaszam > 0
-            // i a játékos szám
-            for (int i = 0; gyufaszam > 0; i++)
-            {
-                Console.WriteLine("Játékos szama: {0}", (i%jatekosszam)+1);
-                int gyufaminusz = AdatBekeres("Add meg hany gyufat vennél el");
-                gyufaszam -= gyufaminusz;
-                Console.WriteLine("Gyufák száma: {0}", gyufaszam);
-                if (gyufaszam <= 0)
-                { 
-                    Console.WriteLine("Vesztes szama: {0}", (i % jatekosszam + 1));
-                }
-            }
-            Console.WriteLine("Játék vége");
-            Console.ReadKey();
-        }
+        private static int gyufaszam = 20;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
         // berkéi a gyufaszálak számát
         private static int AdatBekeres(string szoveg = "Add meg az adatot!")
         {
@@ -48,5 +37,25 @@ namespace GyufaKonya
             }
             return adat;
         }
+
+        private static void Main(string[] args)
+        {
+            // gyufaszámból elveszünk 1/2/3 db-t > input amig a gyufaszam > 0 i a játékos szám
+            for (int i = 0; gyufaszam > 0; i++)
+            {
+                Console.WriteLine("Játékos szama: {0}", (i % jatekosszam) + 1);
+                int gyufaminusz = AdatBekeres("Add meg hany gyufat vennél el");
+                gyufaszam -= gyufaminusz;
+                Console.WriteLine("Gyufák száma: {0}", gyufaszam);
+                if (gyufaszam <= 0)
+                {
+                    Console.WriteLine("Vesztes szama: {0}", (i % jatekosszam + 1));
+                }
+            }
+            Console.WriteLine("Játék vége");
+            Console.ReadKey();
+        }
+
+        #endregion Private Methods
     }
 }
